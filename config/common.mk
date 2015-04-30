e1f41a1fb43fc86d2967f7e0fcdf746f0f562918
vinman12 <dukeman1224@gmail.com>
2015-04-30 09:05:01 -0400

    add LMP bootanimation & Add Gesture Typing lib

--- config/common.mk
+++ config/common.mk
@@ -103,6 +103,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.local.rc:root/init.cm.rc
 
+# Gesture Typing 
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
     vendor/cm/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
